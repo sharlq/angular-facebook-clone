ffd155425fc6c0e94a1627e437c12843e4a85b8f
sharlq <52783044+sharlq@users.noreply.github.com>
2021-08-06 16:39:14 +0300

    initializing the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+    canActivate: []
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
